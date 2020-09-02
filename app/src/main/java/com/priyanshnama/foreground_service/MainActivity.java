120c83bc7dc2ddf6e596f16760eb3d7ae47dcd15
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-09-02 23:47:35 +0530

    initial commit

---
+++ app/src/main/java/com/priyanshnama/foreground_service/MainActivity.java
@@ -0,0 +1,14 @@
+package com.priyanshnama.foreground_service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
